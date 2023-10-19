870ccb68f5dad07e4167d0bf4d99018bbc85bd9c
ImanGuluzade <55985523+ImanGuluzade@users.noreply.github.com>
2023-10-19 22:00:43 +0800

    bits.c

--- bits.c
+++ bits.c
@@ -2,88 +2,68 @@
 #include <limits.h>
 
 int Funct(int arg1, int arg2, ...) {
+    // This function initializes some variables but doesn't perform any specific operation.
     int var1 = arg1;
-
     int varM = arg2;
-
-    int varJ =  0; 
-  
-    int varN =  0; 
-
-    return 0; 
-}
-
-int bitXor(int x, int y) {
-    return ~(x & y) & ~(~x & ~y);
-}
-
-int bitParity(int x) {
-    x = (x >> 1) ^ x;
-    x = (x >> 2) ^ x;
-    x = (x >> 4) ^ x;
-    x = (x >> 8) ^ x;
-    x = (x >> 16) ^ x;
-    return x & 1;
-}
-
-int anyOddBit(int x) {
-    int t = 0xAA;
-    t = t | (t << 8);
-    t = t | (t << 16);
-    return !!(x & t);
-}
-
-int logicalShift(int x, int n) {
-    int t = 1 << (31 - n);
-    return ((x >> n) + t) ^ t;
-}
-
-int rotateRight(int x, int n) {
-    return (x >> n) | ((x & ((1 << n) - 1)) << (32 - n));
-}
-
-int bang(int x) {
-    return ((x | (~x + 1)) >> 31) + 1;
-}
-
-int tmax(void) {
-    return INT_MAX;
-}
-
-int absVal(int x) {
-    int t = x >> 31;
-    return (t ^ (x + t)) - t;
-}
-
-int divpwr2(int x, int n) {
-    int t = x >> 31;
-    return (x + (t & ((1 << n) - 1))) >> n;
-}
-
-int ezFiveEighths(int x) {
-    int y = (x << 2) + x;
-    return (y + ((y >> 31) & 7)) >> 3;
-}
-
-int isPositive(int x) {
-    return !((x >> 31) | !x);
-}
-
-int isLessOrEqual(int x, int y) {
-    return !(((x ^ y) >> 31) | (x + ~y + 1) >> 31);
-}
-
-int ilog2(int x) {
-    int ans = (!(x >> 16)) << 4;
-    ans ^= (!(x >> (ans + 8))) << 3;
-    ans ^= 24;
-    ans ^= (!(x >> (ans + 4))) << 2;
-    x = x >> ans;
-    ans ^= ((~0x3B) >> (x & 30)) & 3;
-    return ans;
+    int varJ = 0;
+    int varN = 0;
+    return 0;
 }
 
 int main() {
-  
+    // Sample usage of the functions:
+    
+    // Example of calling the bitXor function:
+    int result1 = bitXor(5, 3);
+    printf("bitXor result: %d\n", result1);
+
+    // Example of calling the bitParity function:
+    int result2 = bitParity(7);
+    printf("bitParity result: %d\n", result2);
+
+    // Example of calling the anyOddBit function:
+    int result3 = anyOddBit(42);
+    printf("anyOddBit result: %d\n", result3);
+
+    // Example of calling the logicalShift function:
+    int result4 = logicalShift(8, 2);
+    printf("logicalShift result: %d\n", result4);
+
+    // Example of calling the rotateRight function:
+    int result5 = rotateRight(16, 2);
+    printf("rotateRight result: %d\n", result5);
+
+    // Example of calling the bang function:
+    int result6 = bang(0);
+    printf("bang result: %d\n", result6);
+
+    // Example of calling the tmax function:
+    int result7 = tmax();
+    printf("tmax result: %d\n", result7);
+
+    // Example of calling the absVal function:
+    int result8 = absVal(-10);
+    printf("absVal result: %d\n", result8);
+
+    // Example of calling the divpwr2 function:
+    int result9 = divpwr2(16, 2);
+    printf("divpwr2 result: %d\n", result9);
+
+    // Example of calling the ezFiveEighths function:
+    int result10 = ezFiveEighths(20);
+    printf("ezFiveEighths result: %d\n", result10);
+
+    // Example of calling the isPositive function:
+    int result11 = isPositive(-5);
+    printf("isPositive result: %d\n", result11);
+
+    // Example of calling the isLessOrEqual function:
+    int result12 = isLessOrEqual(10, 5);
+    printf("isLessOrEqual result: %d\n", result12);
+
+    // Example of calling the ilog2 function:
+    int result13 = ilog2(32);
+    printf("ilog2 result: %d\n", result13);
+
     return 0;
 }
