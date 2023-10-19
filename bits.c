fa1468600a983eb24d442c6d3dc6ae5954cc5122
ImanGuluzade <55985523+ImanGuluzade@users.noreply.github.com>
2023-10-19 22:13:35 +0800

    bits.c

--- bits.c
+++ bits.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include <limits.h>
 
-int Funct(int arg1, int arg2, ...) {
-    // This function initializes some variables but doesn't perform any specific operation.
-    int var1 = arg1;
-    int varM = arg2;
-    int varJ = 0;
-    int varN = 0;
-    return 0;
-}
+// Function prototypes
+int bitXor(int x, int y);
+int bitParity(int x);
+int anyOddBit(int x);
+int logicalShift(int x, int n);
+int rotateRight(int x, int n);
+int bang(int x);
+int tmax(void);
+int absVal(int x);
+int divpwr2(int x, int n);
+int ezFiveEighths(int x);
+int isPositive(int x);
+int isLessOrEqual(int x, int y);
+int ilog2(int x);
 
 int main() {
     // Sample usage of the functions:
-    
+
     // Example of calling the bitXor function:
     int result1 = bitXor(5, 3);
     printf("bitXor result: %d\n", result1);
@@ -66,4 +71,76 @@ int main() {
     printf("ilog2 result: %d\n", result13);
 
     return 0;
+}
+
+// Implementations of the functions
+
+int bitXor(int x, int y) {
+    return ~(x & y) & ~(~x & ~y);
+}
+
+int bitParity(int x) {
+    x = (x >> 1) ^ x;
+    x = (x >> 2) ^ x;
+    x = (x >> 4) ^ x;
+    x = (x >> 8) ^ x;
+    x = (x >> 16) ^ x;
+    return x & 1;
+}
+
+int anyOddBit(int x) {
+    int t = 0xAA;
+    t = t | (t << 8);
+    t = t | (t << 16);
+    return !!(x & t);
+}
+
+int logicalShift(int x, int n) {
+    int t = 1 << (31 ^ n);
+    return ((x >> n) + t) ^ t;
+}
+
+int rotateRight(int x, int n) {
+    return (x >> n) ^ ((x ^ (x >> 31)) << (31 ^ n) << 1);
+}
+
+int bang(int x) {
+    return ((x | ((~x) + 1)) >> 31) + 1;
+}
+
+int tmax(void) {
+    return ~(1 << 31);
+}
+
+int absVal(int x) {
+    int t = x >> 31;
+    return (t + x) ^ t;
+}
+
+int divpwr2(int x, int n) {
+    int t = x >> 31;
+    return (x + (t ^ (t << n))) >> n;
+}
+
+int ezFiveEighths(int x) {
+    int y = (x << 2) + x;
+    return (y + ((y >> 31) & 7)) >> 3;
+}
+
+int isPositive(int x) {
+    return !(((x >> 31) | (!x)));
+}
+
+int isLessOrEqual(int x, int y) {
+    return ((x + ~(((x ^ y) >> 31) | y)) >> 31) & 1;
+}
+
+int ilog2(int x) {
+    int ans = (!(x >> 16)) << 4;
+    ans ^= (!(x << ans >> 24)) << 3;
+    ans ^= 28;
+    ans ^= (!(x >> ans)) << 2;
+    x = x >> ans;
+    ans ^= ((~0x5B) >> (x & 30)) & 3;
+    return ans;
 }
